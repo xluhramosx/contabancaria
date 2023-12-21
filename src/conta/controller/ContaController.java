b7a2fae52f00906b0607e0542b48aab49c2f2bc6
Luciana Ramos <ramos.d.luciana@gmail.com>
2023-12-20 22:11:45 -0300

    Finalização do Projeto Conta Corrente

--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -60,19 +60,44 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+        var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			if(conta.sacar(valor)== true)
+				System.out.println("\nO Saque na Conta número: " + numero + " foi efetuado com sucesso!");
+		} else {
+			System.out.println("\nA conta número: " + numero + " não foi encontrada!");
+		}
 		
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
+        var conta = buscarNaCollection(numero);
+		
+		if (conta != null) {
+			conta.depositar(valor);
+				System.out.println("\nDepósito na Conta número: " + numero + " foi efetuado com sucesso!");
+		} else {
+			System.out.println("\nA conta número: " + numero + " não foi encontrada ou a Conta destino não é uma Conta Corrente!");
+		}
 		
 	}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
+        var contaOrigem = buscarNaCollection(numeroOrigem);
+        var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if (contaOrigem != null && contaDestino != null) {
+			
+			if (contaOrigem.sacar(valor)== true) {
+				contaDestino.depositar(valor);
+				System.out.println("\nAtransferência foi efetuada com sucesso");
+			}
+		} else {
+			System.out.println("\nA conta de Origem e/ou Destino não foram encontradas");
+		}
 		
 	}
 	
